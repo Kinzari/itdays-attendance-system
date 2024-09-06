5390e696a7f44289eb92bae628e2057aa12c10ed
Kinzari <richkeiser7@gmail.com>
2024-09-06 18:53:12 +0800

    Added attendance_archive table and event_status table

--- it_days_attendance.sql
+++ it_days_attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2024 at 07:43 PM
+-- Generation Time: Sep 06, 2024 at 12:51 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,13 +42,6 @@ CREATE TABLE `attendance` (
   `day` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`id`, `student_id`, `first_name`, `middle_name`, `family_name`, `suffix`, `year_level`, `tribu`, `check_in`, `check_out`, `status`, `day`) VALUES
-(41, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '01:42:04', '01:42:13', 'Complete', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -57,33 +50,34 @@ INSERT INTO `attendance` (`id`, `student_id`, `first_name`, `middle_name`, `fami
 
 CREATE TABLE `attendance_archive` (
   `id` int(11) NOT NULL,
-  `student_id` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `middle_name` varchar(255) DEFAULT NULL,
-  `family_name` varchar(255) NOT NULL,
-  `suffix` varchar(255) DEFAULT NULL,
-  `year_level` varchar(255) NOT NULL,
-  `tribu` varchar(255) NOT NULL,
-  `check_in` time DEFAULT NULL,
+  `student_id` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `family_name` varchar(50) NOT NULL,
+  `suffix` varchar(10) DEFAULT NULL,
+  `year_level` varchar(20) NOT NULL,
+  `tribu` varchar(50) NOT NULL,
+  `check_in` time NOT NULL,
   `check_out` time DEFAULT NULL,
-  `status` enum('Complete','Incomplete') NOT NULL,
-  `day` int(11) NOT NULL,
-  `archived_on` timestamp NOT NULL DEFAULT current_timestamp()
+  `status` varchar(20) DEFAULT NULL,
+  `day` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `attendance_archive`
 --
 
-INSERT INTO `attendance_archive` (`id`, `student_id`, `first_name`, `middle_name`, `family_name`, `suffix`, `year_level`, `tribu`, `check_in`, `check_out`, `status`, `day`, `archived_on`) VALUES
-(1, '02-2021-00143', 'James', '', 'Antipuesto', '', '3rd Year', 'Fighter', '04:07:24', '04:44:05', 'Complete', 1, '2024-09-05 17:26:03'),
-(2, '02-1920-10045', 'Rasidin', '', 'Ansao', '', '3rd Year', 'Support', '14:43:37', '14:48:49', 'Complete', 1, '2024-09-05 17:26:03'),
-(3, '02-2425-00003', 'Pitok', 'Kulas', 'Batolata', '', '4th Year', 'Assassin', '14:47:43', '14:48:06', 'Complete', 1, '2024-09-05 17:26:03'),
-(4, '02-2223-01945', 'Laurenz', '', 'Anchez', '', '3rd Year', 'Jungle', '20:26:14', '20:26:27', 'Complete', 1, '2024-09-05 17:26:03'),
-(5, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '01:10:03', '01:10:27', 'Complete', 1, '2024-09-05 17:26:03'),
-(8, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '01:29:21', '01:30:27', 'Complete', 2, '2024-09-05 17:30:37'),
-(9, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '01:35:32', '01:35:54', 'Complete', 1, '2024-09-05 17:36:07'),
-(10, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '01:38:01', '01:38:11', 'Complete', 2, '2024-09-05 17:39:09');
+INSERT INTO `attendance_archive` (`id`, `student_id`, `first_name`, `middle_name`, `family_name`, `suffix`, `year_level`, `tribu`, `check_in`, `check_out`, `status`, `day`) VALUES
+(16, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '16:06:49', '16:06:53', 'Complete', 1),
+(17, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '16:09:44', NULL, 'Incomplete', 2),
+(18, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '16:49:57', NULL, 'Incomplete', 3),
+(19, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '17:38:31', '17:38:35', 'Complete', 4),
+(20, '02-2021-00143', 'James', '', 'Antipuesto', '', '3rd Year', 'Fighter', '17:39:52', '17:40:07', 'Complete', 4),
+(22, '02-2021-00143', 'James', '', 'Antipuesto', '', '3rd Year', 'Fighter', '17:54:38', '17:54:46', 'Complete', 5),
+(23, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '3rd Year', 'Mage', '17:55:15', '17:55:19', 'Complete', 5),
+(25, '02-2223-01741', 'Rudgel', 'Sales', 'Tagaan', '', '3rd Year', 'Fighter', '18:00:19', '18:00:38', 'Complete', 6),
+(26, '02-2223-03206', 'Van Morre', '', 'Guangco', '', '3rd Year', 'Mage', '18:01:41', '18:01:48', 'Complete', 6),
+(27, '02-2425-12345', 'Pitok', 'Kulas', 'Batolata', '', '3rd Year', 'Magic', '18:15:04', '18:17:02', 'Complete', 6);
 
 -- --------------------------------------------------------
 
@@ -101,7 +95,7 @@ CREATE TABLE `event_status` (
 --
 
 INSERT INTO `event_status` (`id`, `current_day`) VALUES
-(1, 1);
+(1, 7);
 
 -- --------------------------------------------------------
 
@@ -134,8 +128,13 @@ INSERT INTO `students` (`id`, `student_id`, `first_name`, `middle_name`, `family
 (21, '02-2223-03206', 'Van Morre', 'Acaylar', 'Guangco', '', '09582749102', 3, 4, '$2y$10$swIffWAlSTdzJdRJug2QZeZcI23fzOjzpjx5O/SuFMe0mgOHTm1Hm', 'vaac.guangco.coc@phinmaed.com', 'student'),
 (22, '02-2223-05976', 'Jerimiah Exequiel', '', 'Espartero', '', '09157079861', 3, 7, '$2y$10$hm8oH5IUlXwLOWm5ON97Med6DWwT49/DHV4K5xTFSmAu2ctGU06cO', 'jeex.espartero.coc@phinmaed.com', 'sbo'),
 (23, '02-2021-00143', 'James', '', 'Antipuesto', '', '09382901129', 3, 2, '$2y$10$hZ0ejsdlFAran2cRWGho9uX048xUbCUoOkifXR8OSJbzB/tV20JWS', 'ja.antipuesto.coc@phinmaed.com', 'student'),
-(24, '02-2425-00003', 'Pitok', 'Kulas', 'Batolata', '', '09123456789', 4, 1, '$2y$10$iOuF3STiya9KOwtrGHBLluEgAMXj5ICmLEPdL3XVJ16dbrLh4jS3i', 'pitok.batolata.coc@phinmaed.com', 'student'),
-(25, '02-2223-01945', 'Laurenz', '', 'Anchez', '', '09574920122', 3, 3, '$2y$10$N.TTAHhfgJ4nr.8/R7lz4OtR9Tv3raxJYrccEE.BlMxIlCxGBGYeq', 'la.anchez.coc@phinmaed.com', 'student');
+(25, '02-2223-01945', 'Laurenz', '', 'Anchez', '', '09574920122', 3, 3, '$2y$10$N.TTAHhfgJ4nr.8/R7lz4OtR9Tv3raxJYrccEE.BlMxIlCxGBGYeq', 'la.anchez.coc@phinmaed.com', 'student'),
+(26, '02-1516-01541', 'Vince Jolio', 'Tancongco', 'Tillo', '', '09213148551', 3, 1, '$2y$10$cnHF9./ZX4QRyvFb/FtHmuCjS21uto60a8uCuOKGRSfP1c7Mh/lci', 'vita.tillo.coc@phinmaed.com', 'student'),
+(27, '02-2021-01409', 'Mar Louis', 'Pimentel', 'Go', '', '09663348752', 3, 4, '$2y$10$2bx6U77OzkyS93Ay4PHVxuZFHisDFAMcO7BjJwGnD7Cda..g2E3g6', 'mapi.go.coc@phinmaed.com', 'student'),
+(28, '02-2223-10440', 'Andrei', 'P', 'Gopher', 'G', '0932343232', 3, 7, '$2y$10$ZRf8FfZpYrtfuaTUW7Yjj.WJHyKPLmBH86jygQxqKiJEKpICAyflq', 'mela.coc@phinmaed.com', 'student'),
+(29, '02-2021-00344', 'Kristopher', 'Viado', 'Dichos', '', '09534704117', 3, 1, '$2y$10$VMAD3.Mi3Hy0PAyU.brVpuDaPRlUyIwYLAQB4b2QQax32ghhwlT5m', 'krvi.dichos.coc@phinmaed.com', 'student'),
+(30, '02-2223-01741', 'Rudgel', 'Sales', 'Tagaan', '', '09248998990', 3, 2, '$2y$10$nIIqukp1Opv0lzXfy/BghO0GmX47SsJIHyJYCPPg309paKxKTnLpm', 'rusa.tagaan.coc@phinmaed.com', 'student'),
+(31, '02-2425-12345', 'Pitok', 'Kulas', 'Batolata', '', '09123525245', 3, 5, '$2y$10$6j/jzjAZTsnjLjvUc1BuLeu4phYgKDbTwGZISyRpAI0bSQqgncKz.', 'kulas.coc@phinmaed.com', 'student');
 
 -- --------------------------------------------------------
 
@@ -235,25 +234,25 @@ ALTER TABLE `year_levels`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- AUTO_INCREMENT for table `attendance_archive`
 --
 ALTER TABLE `attendance_archive`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `event_status`
 --
 ALTER TABLE `event_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `tribus`
